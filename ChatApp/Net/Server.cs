d3ac98d3d48971807fd5ff54ab6c0c34c0f9f86a
JakubSzwedowicz <kuba.szwedowicz@gmail.com>
2024-05-02 20:04:55 +0200

    Add functionality of connecting to the server from the client

---
+++ ChatApp/Net/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace ChatClient.Net
+{
+    internal class Server
+    {
+        private readonly TcpClient _client = new();
+
+        public void ConnecToServer()
+        {
+            if(!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+            }
+        }
+    }
+}
