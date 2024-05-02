9e1f8855ca471b242770c2286c35cd235b6ca10b
JakubSzwedowicz <kuba.szwedowicz@gmail.com>
2024-05-02 20:20:17 +0200

    Add connecting to the server

---
+++ ChatServer/Client.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+
+namespace ChatServer
+{
+    internal class Client
+    {
+        public string Username { get; set; } = "ClientUsernamePlaceholder";
+        public Guid UID { get; set; }
+        public TcpClient ClientSocket { get; set; }
+
+        public Client(TcpClient client)
+        {
+            ClientSocket = client;
+            UID = Guid.NewGuid();
+
+            Console.WriteLine($"[{DateTime.Now}]: Client has connected with username: {Username}");
+        }
+    }
+}
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    internal class Program
+    {
+        private static List<Client> _users;
+        private static TcpListener _listener;
+
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+                 Console.WriteLine("Client has connected!");
+
+                // Broadcast the connection to everyone on the server
+            }
+        }
+    }
+}
